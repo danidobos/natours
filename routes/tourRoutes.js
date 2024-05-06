5b6907b4fb0ad64b3ccb26020f7019efb95e11be
danidobos <danidobos97@gmail.com>
2024-05-06 23:46:57 +0200

    Chaining multiple middlewares

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,10 +7,13 @@ const router = express.Router();
 // In this case the callback fn has a 4th param --> the value that we are looking for
 router.param('id', tourController.checkID);
 
+// We can "chain" multiple middlewares in a route fe.: before create, we check, if the
+// req. body contains name and price, if yes only then create the tour
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
+
 router
   .route('/:id')
   .get(tourController.getTour)
