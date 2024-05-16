e03889a011f5842aaaa5cd54355ba50510a909ab
danidobos <danidobos97@gmail.com>
2024-05-16 15:07:34 +0200

    Delete document with mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -76,12 +76,20 @@ const updateTour = async (req, res) => {
   }
 };
 
-const deleteTour = (req, res) => {
-  const id = req.params.id * 1;
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 module.exports = {
