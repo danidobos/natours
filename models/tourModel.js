c1c54dd42269acc404a17512599ea4fa8ce6de0f
danidobos <danidobos97@gmail.com>
2024-06-12 16:49:22 +0200

    Modelling tour guides: Child referencing

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
-const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -103,7 +102,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -117,13 +121,6 @@ tourSchema.virtual('durationWeeks').get(function () {
 
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
-  next();
-});
-
-tourSchema.pre('save', async function (next) {
-  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-
   next();
 });
 
